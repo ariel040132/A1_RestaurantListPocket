ace71643b512ed393d7eae6d450e0ae6499e029c
Ariel <ariel040132@gmail.com>
2023-03-31 19:21:52 +0800

    ver. finished

--- app.js
+++ app.js
@@ -1,43 +1,118 @@
-// require packages used in the project
+//! 載入所需工具包
+const mongoose = require("mongoose"); // 載入 mongoose
 const express = require("express");
 const app = express();
 const port = 3000;
 const exphbs = require("express-handlebars");
-const restaurantList = require("./restaurant.json");
+const Restaurant = require("./models/Restaurants");
 
-//載入之後，要告訴 Express：麻煩幫我把樣板引擎交給 express-handlebars：
+//! 資料庫設定
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+//! 樣板引擎設定
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-//產生靜態檔案
 app.use(express.static("public"));
-
-// routes setting
+//呼叫 body-parser
+app.use(express.urlencoded({ extended: true }));
+//! 首頁 - finished
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  Restaurant.find({})
+    .lean()
+    .then((restaurantData) => {
+      res.render("index", { restaurantData });
+    })
+    .catch((err) => console.log(err));
+});
+//! 新增餐廳 - finished
+app.get("/restaurants/new", (req, res) => {
+  res.render("new");
+});
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
 
-//show 頁面
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  const restaurants = restaurantList.results.find(
-    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
-  );
-  res.render("show", { restaurants });
+//! 餐廳詳情- finished
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  Restaurant.findById(id)
+    .lean()
+    .then((restaurants) => {
+      res.render("show", { restaurants });
+    })
+    .catch((err) => console.log(error));
 });
 
-//search function
+//! 搜尋餐廳
 app.get("/search", (req, res) => {
-  const keyword = req.query.keyword;
-  const restaurants = restaurantList.results.filter((restaurant) => {
-    return (
-      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-    );
-  });
-  res.render("index", { restaurants, keyword });
+  if (!req.query.keywords) {
+    res.redirect("/");
+  }
+  const keywords = req.query.keywords;
+  const keyword = req.query.keywords.trim().toLowerCase();
+
+  Restaurant.find({})
+    .lean()
+    .then((restaurantData) => {
+      const filterData = restaurantData.filter((data) => {
+        // data.name & data.category 都有顯示
+        return (
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+        );
+      });
+      res.render("index", { restaurantData: filterData, keywords });
+    })
+    .catch((err) => console.log(err));
 });
 
-// start and listen on the Express server
+//! 編輯餐廳 - finished
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  Restaurant.findById(id)
+    .lean()
+    .then((restaurantData) => {
+      res.render("edit", { restaurantData });
+    })
+    .catch((err) => console.log(err));
+});
+app.post("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  Restaurant.findByIdAndUpdate(id, req.body)
+    .then((result) => {
+      console.log(result);
+      res.redirect(`/restaurants/${id}`);
+    })
+    .catch((err) => console.log(err));
+});
+
+//! 刪除餐廳 - finished
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id;
+  Restaurant.findByIdAndRemove(id)
+    .lean()
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+//! start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
 });
