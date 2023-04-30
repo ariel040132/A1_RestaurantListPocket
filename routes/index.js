4eb7ad4dd81afd8e0e1c8a1d7c47209933d0ac4f
Ariel <ariel040132@gmail.com>
2023-04-30 18:39:01 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const home = require("./modules/home");
 const RestRoute = require("./modules/RestRoute");
+const users = require("./modules/users"); // add this
+
 router.use("/", home);
 router.use("/restaurants", RestRoute);
+router.use("/users", users); // add this
 
 module.exports = router;
