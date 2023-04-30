2ae66dbba0b1289a7bdd48e89f593b4f94825600
Ariel <ariel040132@gmail.com>
2023-04-30 21:33:30 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/users");
+//!登入Get
 router.get("/login", (req, res) => {
   res.render("login");
 });
+//!登入Post
 router.post("/login", (req, res) => {});
+//!註冊Get
 router.get("/register", (req, res) => {
   res.render("register");
 });
+//!註冊Post
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log("This email is existed.");
+        res.render("register", {
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      } else {
+        return User.create({ name, email, password })
+          .then(() => res.redirect("/"))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+//!底部
 module.exports = router;
