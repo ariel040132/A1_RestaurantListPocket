613bf20e75f5df1aa3463a4d4e5eca133a2be313
Ariel <ariel040132@gmail.com>
2023-05-01 20:06:14 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -4,20 +4,37 @@ if (process.env.NODE_ENV !== "production") {
 const Restaurant = require("../Restaurants");
 const restaurantList = require("../../restaurant.json").results;
 const db = require("../../config/mongoose");
-
+const bcrypt = require("bcryptjs");
+const User = require("../../models/users");
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
 db.once("open", () => {
-  console.log("running restaurantSeeder script...");
-  Restaurant.create(restaurantList)
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      const restaurantListWithUserId = restaurantList.map((restaurant) => ({
+        ...restaurant,
+        userId,
+      }));
+      return Restaurant.create(restaurantListWithUserId);
+    })
     .then(() => {
       console.log("RestaurantSeeder done!");
-      db.close();
+      process.exit();
     })
     .catch((err) => console.log(err));
-
-  // Restaurant.create(restaurantList)
-  //   .then(() => {
-  //     console.log("restaurantSeeder done!");
-  //     db.close();
-  //   })
-  //   .catch((err) => console.log(err));
 });
+// console.log("running restaurantSeeder script...");
+//   Restaurant.create(restaurantList)
